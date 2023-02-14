97b0d8af419b10df113177200b7dcf1caf944db5
saanghyuk <saanghyuk@gmail.com>
2023-02-14 13:05:38 +0900

    Before 20230214 session

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+function closure() {
+  let cnt = 0;
+  function cntPlus() {
+    cnt = cnt + 1;
+  }
+
+  function printCnt() {
+    console.log(cnt);
+  }
+
+  return cntPlus;
+}
+
+const cntClosure = closure();
+
+console.log(cntClosure);
+cntClosure();
+
+console.log(cnt);
